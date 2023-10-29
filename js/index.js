36c780c24798abe11f13c65737d356bb436abb8c
YOGESH CHANDRA <yogeshchandra52@gmail.com>
2023-10-29 18:27:44 +0530

    Project finally completed

--- js/index.js
+++ js/index.js
@@ -1,28 +1,23 @@
-
-
-
-
 // Slider/Card control
 
 let cards = document.querySelectorAll(".card");
 let counter = 0;
 let screenWidth = screen.width;
 
-
-const cardX = (screenWidth)=>{
+const cardX = (screenWidth) => {
   cards.forEach((card, i) => {
     if (screenWidth > 768) {
-      card.style.left = `${((i * screenWidth) / 4)}px`;
+      card.style.left = `${(i * screenWidth) / 4}px`;
       card.style.width = `${screenWidth / 4}px`;
     } else if (screenWidth < 577) {
-      card.style.left = `${(i * screenWidth)}px`;
-      card.style.width = `${screenWidth }px`;
+      card.style.left = `${i * screenWidth}px`;
+      card.style.width = `${screenWidth}px`;
     } else {
       card.style.left = `${(i * screenWidth) / 2}px`;
       card.style.width = `${screenWidth / 2}px`;
     }
   });
-}
+};
 
 cardX(screenWidth);
 
@@ -40,21 +35,16 @@ const goPrev = () => {
   }
 };
 const goNext = () => {
-
   if (
-    (counter == cards.length-4 && screenWidth > 768) 
-    ||
-    ((counter == cards.length-2 ) && (screenWidth < 769 || screenWidth > 576)) 
-    ||
-    (counter == cards.length && screenWidth < 577)
+    ((counter == cards.length - 3 && screenWidth > 768) ||
+    (counter == cards.length - 1 && (screenWidth < 769 || screenWidth > 576)) ||
+    (counter == cards.length && screenWidth < 577))
   ) {
     counter = 0;
- 
-  }
-  else{
+  } else {
+    console.log("counter:", counter);
+    slideImage();
     counter++;
-
-  slideImage();
   }
 };
 
